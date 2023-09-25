565cf57ce4b00ddf3e0a99e6972a37cd321da023
Luca P <lucaexequiel@gmail.com>
2023-09-25 13:53:03 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-// import { userRouter } from './routes/user.routes.ts'
+import { userRouter } from './routes/user.routes.js'
 
 const app = express()
 app.use(express.json())
 
-// app.use('/api/users', userRouter)
+// app.use('/api/?', ?)
 
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resource not found' })
