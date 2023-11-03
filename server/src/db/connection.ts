6e088a05c51cbcde9db5b3a578cdeff76ff432c3
alonsocami <alonsocami.a@gmail.com>
2023-11-03 19:56:51 -0300

    cambio de bd

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize('dsw_cine', 'root', 'root', {
+const sequelize = new Sequelize('cine', 'cine', 'cine', {
   host: 'localhost',
   dialect: "mysql",
   //logging:false,  si quisiera que no aparezca el SELECT 1+1
