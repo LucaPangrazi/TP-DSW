8875a402d552aca484c40c59d5f2b3c695b14221
alonsocami <alonsocami.a@gmail.com>
2023-11-17 12:05:27 -0300

    .

--- server/dist/db/connection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('cine', 'cine', 'cine', {
-    host: 'localhost',
-    dialect: "mysql",
-    //logging:false,  si quisiera que no aparezca el SELECT 1+1
-});
-exports.default = sequelize;
