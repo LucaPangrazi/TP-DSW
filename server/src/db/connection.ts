cf6605b216eec309f2b793acdbcd9abb8eeb705a
alonsocami <alonsocami.a@gmail.com>
2023-11-22 08:59:25 -0300

    actualizo

---
+++ server/src/db/connection.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize";
+
+
+const sequelize = new Sequelize('cine', 'cine', 'cine', {
+  host: 'localhost',
+  dialect: "mysql",
+  //logging:false,  si quisiera que no aparezca el SELECT 1+1
+});
+
+export default sequelize
