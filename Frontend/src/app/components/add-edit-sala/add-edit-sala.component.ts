0495e35f8f8074349621ac0724b054e7c7f31c46
alonsocami <alonsocami.a@gmail.com>
2023-12-02 14:52:42 -0300

    cambio ruta (botones de sala)

--- Frontend/src/app/components/add-edit-sala/add-edit-sala.component.ts
+++ Frontend/src/app/components/add-edit-sala/add-edit-sala.component.ts
@@ -10,71 +10,67 @@ import { SalaService } from '../../services/sala.service';
   templateUrl: './add-edit-sala.component.html',
   styleUrls: ['./add-edit-sala.component.css']
 })
-export class AddEditSalaComponent {
-  form:FormGroup;
+export class AddEditSalaComponent implements OnInit {
+  form: FormGroup;
   loading: boolean = false;
-   id: number;
+  id: number;
   operacion: string = 'Agregar ';
 
-  constructor(private fb:FormBuilder,
+  constructor(
+    private fb: FormBuilder,
     private _salaService: SalaService,
     private router: Router,
     private toastr: ToastrService,
     private aRouter: ActivatedRoute,
-   ) {
+  ) {
     this.form = this.fb.group({
       //id:['',Validators.required], creo q este no va
-      name:['',Validators.required]
-    })
-     this.id = Number(aRouter.snapshot.paramMap.get('id'));
+      name: ['', Validators.required]
+    });
+    this.id = Number(aRouter.snapshot.paramMap.get('id'));
   }
 
-  ngOnInit():void {
- if (this.id != 0) {
-//       // Es editar
-       this.operacion = 'Editar ';
-       this.getSala(this.id);
-     }
+  ngOnInit(): void {
+    if (this.id !== 0) {
+      this.operacion = 'Editar ';
+      this.getSala(this.id);
+    }
   }
 
   getSala(id: number) {
-     this.loading = true;
-     this._salaService.getSala(id).subscribe((data: Sala) => {
-       this.loading = false;
-       this.form.setValue({
-         name: data.name, 
-       })
-     })
-   }
+    this.loading = true;
+    this._salaService.getSala(id).subscribe((data: Sala) => {
+      this.loading = false;
+      this.form.setValue({
+        name: data.name,
+      });
+    });
+  }
 
   addSala() {
-    /*  console.log(this.form.value.name);
-     console.log(this.form.get('name')?.value); */
+    const sala: Sala = {
+      id: this.id,
+      name: this.form.value.name
+    };
 
-     const sala: Sala = {
-     id: this.form.value.id,   //ver si afecta si lo dejo
-     name:this.form.value.name
-}
+    this.loading = true;
 
-  this.loading = true;
-
-     if (this.id !== 0) {
-//       // Es editar 
-       sala.id = this.id;
-this._salaService.updateSala(this.id, sala).subscribe(() => {
+    if (this.id !== 0) {
+      this._salaService.updateSala(this.id, sala).subscribe(() => {
         this.toastr.info(`La sala ${sala.name} fue actualizada con éxito`, 'Sala actualizada');
         this.loading = false;
-        this.router.navigate(['/']);
-      })
-
+        this.navigateToSalas();  // Cambio aquí
+      });
     } else {
-       // Es agregar
-       this._salaService.saveSala(sala).subscribe(() => {
-         this.toastr.success(`La sala ${sala.name} fue registrada con éxito`, 'Sala registrada');
-         this.loading = false;
-         this.router.navigate(['/']);
-       })
+      this._salaService.saveSala(sala).subscribe(() => {
+        this.toastr.success(`La sala ${sala.name} fue registrada con éxito`, 'Sala registrada');
+        this.loading = false;
+        this.navigateToSalas();  // Cambio aquí
+      });
+    }
+  }
 
-}
-}
+  navigateToSalas() {
+    this.router.navigate(['/salas']);
+  }
 }
