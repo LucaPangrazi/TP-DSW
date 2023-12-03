51ba4ed2e5172ebebf94a4a0becc7b604d402c38
LucaPangrazi <114444871+LucaPangrazi@users.noreply.github.com>
2023-12-03 12:13:26 -0300

    Update login.component.ts

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -56,7 +56,7 @@ export class LoginComponent implements OnInit {
     this._userService.login(user).subscribe({
       next: (token) => {
         localStorage.setItem('token', token);
-        this.router.navigate(['/dashboard'])
+        this.router.navigate(['/home'])
       },
       error: (e: HttpErrorResponse) => {
         this._errorService.msjError(e);
@@ -64,7 +64,4 @@ export class LoginComponent implements OnInit {
       }
     })
   }
-
-  
-
 }
