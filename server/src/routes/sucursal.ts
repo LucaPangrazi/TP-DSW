96b34328943ea6ec79a3bbc78b33bb76d66bf773
Eliasarce98 <eliasarce9859@gmail.com>
2023-12-05 12:31:56 -0300

    ult cambios
    ult cambios

--- server/src/routes/sucursal.ts
+++ server/src/routes/sucursal.ts
@@ -2,6 +2,8 @@ import {Router} from 'express';
 import { deleteSucursal, getSucursal, getSucursales, postSucursal, updateSucursal } from '../controllers/sucursal';
 
 const routerS = Router();
+
+
 routerS.get('/', getSucursales)
 routerS.get('/:id', getSucursal)
 routerS.delete('/:id', deleteSucursal)
