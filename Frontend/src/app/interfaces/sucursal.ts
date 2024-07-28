fe9a85830513e3a15b488aa480b66fcf1966f45a
alonsocami <alonsocami.a@gmail.com>
2024-07-28 12:13:08 -0300

    .

--- Frontend/src/app/interfaces/sucursal.ts
+++
@@ -1,7 +0,0 @@
-export interface Sucursal {
-
-    id?:number,
-    nombre:string,
-    localidad:string,
-    email:string 
-}
--- server/src/conexiones/db.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-
-const db = new Sequelize('cine', 'cine', 'cine', {
-    host: 'localhost',
-    dialect: 'mysql',   
-});
-
-export default db;
