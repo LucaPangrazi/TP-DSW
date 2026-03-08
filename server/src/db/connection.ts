ff6c1daa6308e8168afbb932c0806c94e42ed892
Elias Arce <elias.arce@gmail.com>
2026-03-08 13:11:11 -0300

    fix: prueba user railway en vez de root

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 // Forzamos la URL pública que copiaste de Railway
-const publicUrl = 'mysql://root:CKeh0vxkuAwFtkjnRoxQFihpeoJPIcHB@centerbeam.proxy.rlwy.net:29098/railway';
+const publicUrl = 'mysql://railway:CKeh0vxkuAwFtkjnRoxQFihpeoJPIcHB@centerbeam.proxy.rlwy.net:29098/railway';
 
 console.log("LOG DE CONTROL: Intentando conectar con URL Hardcodeada v2");
 const sequelize = new Sequelize(publicUrl, {
