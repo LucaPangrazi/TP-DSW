b3e8de8f47629deac295367402403515c21bc572
Elias Arce <elias.arce@gmail.com>
2026-03-08 12:33:32 -0300

    fix: hardcoding public url para debug final

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,22 +1,15 @@
 import { Sequelize } from "sequelize";
-import 'dotenv/config';
 
-// Usamos la MYSQL_PUBLIC_URL que copiaste de Railway
-// Esta URL ya contiene: usuario, contraseña, host, puerto y nombre de la base de datos.
-const databaseUrl = process.env.DATABASE_URL || 'mysql://root:CKeh0vxkuAwFtkjnRoxQFihpeoJPIcHB@centerbeam.proxy.rlwy.net:29098/railway';
+// Forzamos la URL pública que copiaste de Railway
+const publicUrl = 'mysql://root:CKeh0vxkuAwFtkjnRoxQFihpeoJPIcHB@centerbeam.proxy.rlwy.net:29098/railway';
 
-console.log("DEBUG: Intentando conectar a la base de datos de Railway...");
-
-const sequelize = new Sequelize(databaseUrl, {
+const sequelize = new Sequelize(publicUrl, {
   dialect: "mysql",
   dialectOptions: {
     ssl: {
-      // Esto es fundamental para conexiones externas a Railway
       rejectUnauthorized: false
     }
-  },
-  // Opcional: Para ver las consultas en la consola de Render
-  logging: false 
+  }
 });
 
 export default sequelize;
