73897922f4f3c545d22e26319d730eb7ec0d4243
Elias Arce <elias.arce@gmail.com>
2026-03-08 13:04:21 -0300

    fix: forzando url directa y log de control

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 // Forzamos la URL pública que copiaste de Railway
 const publicUrl = 'mysql://root:CKeh0vxkuAwFtkjnRoxQFihpeoJPIcHB@centerbeam.proxy.rlwy.net:29098/railway';
 
+console.log("LOG DE CONTROL: Intentando conectar con URL Hardcodeada v2");
 const sequelize = new Sequelize(publicUrl, {
   dialect: "mysql",
   dialectOptions: {
